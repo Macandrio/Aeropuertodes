4a93553ab465375b07f58bfffa1f39906b259fc9
Alejandro Gomez <alejandro.gomez.rivera.al@iespoligonosur.org>
2025-01-28 11:56:15 +0100

    Nada

--- Aeropuerto_res/apaeropuerto/api_urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from  .api_views import *
-
-urlpatterns = [
-    path('Aeropuerto',lista_aeropuerto, name='lista_aeropuerto'),
-    path('Aerolinea',lista_aerolinea, name='lista_aerolinea'),
-    path('Vuelo',lista_vuelo, name='lista_vuelo'),
-]
